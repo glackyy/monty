36cca34c25f7fe058ef31dad0dcf075619e0a3da
glackyy <glackycode@example.com>
2023-06-21 08:55:11 -0700

    hotfix

--- pop.c
+++ pop.c
@@ -6,8 +6,8 @@
  */
 void pop(stack_t **s, unsigned int l_num)
 {
-stack_t *curr = NULL
-if (!*stack || !stack)
+stack_t *curr = NULL;
+if (!*s || !s)
 {
 	fprintf(stderr, "L%d: can't pop an empty stack\n", l_num);
 	free_all();
